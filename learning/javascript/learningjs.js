21fb51e0af237ae0caa39a60597305757473bd9f
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2022-11-07 20:21:11 +0530

    learning evenlistrners

--- learning/javascript/learningjs.js
+++ learning/javascript/learningjs.js
@@ -1,6 +1,6 @@
-var borderToGreen = document.getElementById("demo");
-borderToGreen.style.border="1px solid green";
-var borderToBlue=document.querySelectorAll(".demo");
-borderToBlue[0].style.border="1px solid blue";
-borderToBlue[1].style.border="1px solid blue";
-borderToBlue[1].innerText="second element of the class";
+function alertThrower(message)
+{
+    alert(message);
+}
+var hellobtn = document.getElementById('btn');
+hellobtn.addEventListener('click',alertThrower ('hello') ,);
