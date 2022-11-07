6fe6293bc282077fd19120867142e8a906e66478
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2022-11-07 05:48:44 +0530

    dom q

---
+++ learning/javascript/learningjs.js
@@ -0,0 +1,6 @@
+var borderToGreen = document.getElementById("demo");
+borderToGreen.style.border="1px solid green";
+var borderToBlue=document.querySelectorAll(".demo");
+borderToBlue[0].style.border="1px solid blue";
+borderToBlue[1].style.border="1px solid blue";
+borderToBlue[1].innerText="second element of the class";
