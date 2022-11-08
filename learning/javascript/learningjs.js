05ee071593155f95aa66a4eb35a9c4ed43f95fcd
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2022-11-08 08:18:12 +0530

    keyCode and key press

--- learning/javascript/learningjs.js
+++ learning/javascript/learningjs.js
@@ -1,6 +1,18 @@
-function alertThrower(message)
-{
-    alert(message);
-}
-var hellobtn = document.getElementById('btn');
-hellobtn.addEventListener('click',alertThrower ('hello') ,);
+// var box = document.getElementById('box');
+// var counter = document.getElementById('counter');
+// count = 0 ;
+// box.addEventListener('click',function(){
+//     count++;
+//     counter.innerText = "box clicked " + count + " times";
+// });
+var box = document.getElementById("box");
+box.addEventListener('mouseover',function(){
+    console.log('mouse Over');
+})
+box.addEventListener('mouseout',function(){
+    console.log('mouse Out');
+})
+var Sinput = document.getElementById('search');
+Sinput.addEventListener('keydown',function(event){
+    console.log('key press',event.keyCode);
+});
