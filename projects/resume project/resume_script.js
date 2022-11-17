5c2f411f759311abb1c8f23fe37842322bda0d99
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2022-11-17 10:35:11 +0530

    added smooth scroll

---
+++ projects/resume project/resume_script.js
@@ -0,0 +1,25 @@
+var navMenuAnchorTags = document.querySelectorAll('.nav-menu a')
+// console.log(navMenuAnchorTags)
+var interval;
+for(var i=0;i<navMenuAnchorTags.length;i++)
+{
+    navMenuAnchorTags[i].addEventListener('click',function(event){
+        event.preventDefault();
+        var targetSectionID=this.textContent.trim().toLowerCase();
+        console.log(targetSectionID);
+        var targetSection = document.getElementById(targetSectionID);
+        interval = setInterval(function(){
+            scrollVertically(targetSection);
+        },20);
+    });
+}
+function scrollVertically(targetSection)
+{
+    var targ_coord = targetSection.getBoundingClientRect();
+    if(targ_coord.top<=0)
+    {
+        clearInterval(interval);
+        return;
+    }
+    window.scrollBy(0,50);
+}
