7d37c355eba00238e8366312fa0b19785c8ef955
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2022-11-17 12:13:07 +0530

    added skill fill animation

--- projects/resume project/resume_script.js
+++ projects/resume project/resume_script.js
@@ -1,16 +1,14 @@
+//===================smooth scroll===============================
+
 var navMenuAnchorTags = document.querySelectorAll('.nav-menu a')
-// console.log(navMenuAnchorTags)
 var interval;
 for(var i=0;i<navMenuAnchorTags.length;i++)
 {
     navMenuAnchorTags[i].addEventListener('click',function(event){
         event.preventDefault();
         var targetSectionID=this.textContent.trim().toLowerCase();
-        // console.log(targetSectionID);
         var targetSection = document.getElementById(targetSectionID);
-        interval = setInterval(function(){
-            scrollVertically(targetSection);
-        },20);
+        interval = setInterval(scrollVertically,20,targetSection);
     });
 }
 function scrollVertically(targetSection)
@@ -23,3 +21,59 @@ function scrollVertically(targetSection)
     }
     window.scrollBy(0,50);
 }
+
+//==============skill autofill animation========================
+
+var allSkillProgressBars = document.getElementsByClassName('skill-progress-bar');
+var skillsContainer = document.getElementById('skill-display');
+// console.log(allSkillProgressBars)
+var animationDone = false;
+//reset the inner skill progress bar to 0
+function initialiseBars()
+{
+    for(var bar of allSkillProgressBars)
+    {
+    bar.style.width = 0 + '%';
+    }
+}
+initialiseBars();
+//start animaion on every skill ->increasse skill width from  0 to skill level
+function fillBars()
+{
+    for(let bar of allSkillProgressBars)
+    {
+        let currentWidth = 0;
+        let targetWidth = bar.getAttribute('data-skill-percentage');
+        let barAnimationInterval = setInterval(function(){
+            if(currentWidth>=targetWidth)
+            {
+                clearInterval(barAnimationInterval);
+                return;
+            }
+            currentWidth++;
+            bar.style.width = currentWidth + '%';
+            
+        },20);
+    }
+    animationDone = true;
+}
+// check if skill scontainer is visible
+function reachedSkillSection()
+{
+    var skillsContainerLoc = skillsContainer.getBoundingClientRect().top;
+    if(!animationDone&&skillsContainerLoc<=window.innerHeight)
+    {
+       
+        animationDone = true;
+        fillBars();
+        
+    }
+    else if(skillsContainerLoc>window.innerHeight)
+    {
+        animationDone=false;
+        initialiseBars();
+    }
+}
+// handel scroll event
+window.addEventListener("scroll", reachedSkillSection);
+
