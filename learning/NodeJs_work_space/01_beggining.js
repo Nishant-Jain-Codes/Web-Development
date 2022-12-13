e756dae6ad1b2b1fd21cd0754b1c99fcf5d20da0
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2022-12-13 22:44:37 +0530

    siu

---
+++ learning/NodeJs_work_space/01_beggining.js
@@ -0,0 +1,9 @@
+console.log('hello world :)');
+function add(a,b)
+{
+    return a+b;
+}
+console.log(add(1,2))
+console.log(process.argv);
+var args = process.argv.slice(2);
+console.log('add: ',add(parseInt(args[0]),parseInt(args[1])));
---
+++ learning/NodeJs_work_space/module_node_js/index.js
@@ -0,0 +1,3 @@
+const opp= require('./operations');
+console.log(opp.add(2,3));
+console.log(opp.multiply(2,3));
