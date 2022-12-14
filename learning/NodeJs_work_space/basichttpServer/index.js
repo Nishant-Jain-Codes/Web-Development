29d3114f75b498675018a8d8eb1fbbd9551461f6
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2022-12-14 22:27:23 +0530

    created a server

--- learning/NodeJs_work_space/basichttpServer/index.js
+++ learning/NodeJs_work_space/basichttpServer/index.js
@@ -1,2 +1,19 @@
 const http = require('http');
-console.log(http);
+const port = 8000;
+function requestHandler(req,res)
+{
+    console.log(req.url);
+    res.end('got it ')
+}
+const server = http.createServer(requestHandler);
+server.listen(port,function (error){
+    if(error)
+        {
+            console.log(error);
+            return;
+        }
+    else 
+        {
+            console.log("server is up and running on port : ",port);
+        }
+});
