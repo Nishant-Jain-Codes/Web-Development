7eb8afdde38ed6f7511ccc870f26e6c014c874a7
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-01-06 18:37:17 +0530

    changed the path of the node_modeule repository

--- learning/NodeJs_work_space/contact_list/index.js
+++ learning/NodeJs_work_space/contact_list/index.js
@@ -1,19 +1,28 @@
+//dependencies
 const express = require('express');
+const path = require('path');
+//variables
 const port = 8000;
-const app = express();//app now contains all functionalities of express module
+const app = express();
+//setting properties of app and giving them values app.set(value,property)
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'))
 app.get('/',function(req,res){
-    console.log(req);
-    res.send('its running ');
-})
+    return res.render('home',{
+        'title' : 'my contacts app'
+    });
+});
+app.get('/profile',function(req,res){
+    return res.render('profile');
+});
 app.listen(port,function(error){
     if(error)
     {
-        console.log('error occured : ',error);
-        return;
+        console.log('error occured :-',error);
+        return ;
     }
     else 
     {
-        console.log('server is up and running on port : ',port);
-        return;
+        console.log('server is up and running on port :-',port);
     }
 })
