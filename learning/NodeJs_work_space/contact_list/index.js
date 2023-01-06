80eace7907459a251c3a657e6464b9125b870a26
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-01-06 19:28:21 +0530

    learning backend

--- learning/NodeJs_work_space/contact_list/index.js
+++ learning/NodeJs_work_space/contact_list/index.js
@@ -1,20 +1,64 @@
 //dependencies
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 //variables
 const port = 8000;
 const app = express();
+//contact list data
+let contactList =[
+    {
+        name:'nishant',
+        phone:'1112312'
+    },
+    {
+        name:'ishant',
+        phone:'1231212'
+    },
+    {
+        name:'arun',
+        phone:'21123112'
+    },
+    {
+        name:'varun',
+        phone:'22312312'
+    },
+    {
+        name:'bhem',
+        phone:'12211323'
+    },
+    {
+        name:'aron',
+        phone:'12331231'
+    }
+]
 //setting properties of app and giving them values app.set(value,property)
+
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
+app.use(bodyParser.urlencoded({extended: false}));
 app.get('/',function(req,res){
     return res.render('home',{
-        'title' : 'my contacts app'
+        'title' : 'Contats list',
+        contact_list : contactList
     });
 });
 app.get('/profile',function(req,res){
     return res.render('profile');
 });
+
+
+app.post('/create-contact',function(req,res){
+    contactList.push({
+        name : req.body.name,
+        phone : req.body.phone
+    });
+    return res.redirect('back');
+    // return res.redirect('/');can use this too
+});
+
+
+
 app.listen(port,function(error){
     if(error)
     {
