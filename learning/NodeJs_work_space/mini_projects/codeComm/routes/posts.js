7c9570225c37310b62014bddbd04ab1632f4dba8
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-01-08 18:01:27 +0530

    dfg

--- learning/NodeJs_work_space/mini_projects/codeComm/routes/posts.js
+++ learning/NodeJs_work_space/mini_projects/codeComm/routes/posts.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 const postsController = require('../controllers/posts_controller');
 router.get('/',postsController.posts);
 router.get('/analytics',postsController.analytics);
