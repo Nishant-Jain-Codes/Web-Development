c4def025503c1a75b550c2e1ff6220c0e5981edc
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-01-08 17:05:39 +0530

    making progress

---
+++ learning/NodeJs_work_space/mini_projects/codeComm/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+// * setting up a middle ware which will do the following
+// * routing the home url to all the further url 
+app.use('/',require('./routes/index'));
+
+app.listen(port,function(error){
+    if(error)
+    {
+        console.log(`Error occurred while starting the server ${error}`);
+    }
+    else
+    {
+        console.log(`Successfully started the server on port ${port}`);
+    }
+});
---
+++ learning/NodeJs_work_space/mini_projects/codeComm/routes/index.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const route = express.Router();
+// exporting route for other files to use
+module.exports = route;
