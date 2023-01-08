64d7c6f8bce467ca815cad9952afa8e27c894896
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-01-08 14:26:11 +0530

    starting major project

---
+++ learning/NodeJs_work_space/mini_projects/codeComm/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+app.listen(port,function(error){
+    if(error)
+    {
+        console.log(`Error in running the server : ${error}`);
+        return;
+    }
+    else
+    {
+        console.log(`server is running on ${port}`);
+        return;
+    }
+});
