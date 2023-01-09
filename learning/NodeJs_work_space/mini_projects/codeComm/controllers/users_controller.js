d512a2cf5fc96869910b63350823e8c5b1577d6e
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-01-09 21:45:52 +0530

    created signin sign up pages

--- learning/NodeJs_work_space/mini_projects/codeComm/controllers/users_controller.js
+++ learning/NodeJs_work_space/mini_projects/codeComm/controllers/users_controller.js
@@ -9,3 +9,23 @@ module.exports.profile = function(req,res){
 
     return res.end('<h1>User Profile</h1>');
 }
+//set up sign up action
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title: "codeComm | Sign Up"
+    })
+}
+// set up sign in action
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title: "codeComm | Sign Ip"
+    })
+}
+//get the sign up data
+module.exports.create = function(req,res){
+    //TODO: later
+}
+//action for create session / logging in
+module.exports.createSession = function(req,res){
+    //TODO: later
+}
--- learning/NodeJs_work_space/mini_projects/codeComm/routes/users.js
+++ learning/NodeJs_work_space/mini_projects/codeComm/routes/users.js
@@ -2,5 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users_controller');
 router.get('/',usersController.user);
+router.get('/sign-in',usersController.signIn);
+router.get('/sign-up',usersController.signUp);
 router.get('/profile',usersController.profile);
 module.exports = router;
