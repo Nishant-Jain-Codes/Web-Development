bdb2c29688af3d5e70c1aa9df04eaee320241049
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-01-15 21:55:54 +0530

    using async await making code cleaner

--- learning/NodeJs_work_space/mini_projects/codeComm/controllers/home_controller.js
+++ learning/NodeJs_work_space/mini_projects/codeComm/controllers/home_controller.js
@@ -1,25 +1,25 @@
 const Post = require('../models/post');
 const User= require('../models/user');
-module.exports.home = function(req,res){
-    //populating multiple fields of the post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(error,posts){
-        if(error){console.log('error in finding post to print',error);return;}
-        User.find({},function(error,users){
-            if(error){console.log('error in finding users to print',error);return;}
-            return res.render('home',{
-                title : 'codeComm | Home',
-                all_posts: posts,
-                all_users: users
-            });
-        });
-        // return res.redirect('back');
-    });
+module.exports.home = async function(req,res){
+    try{
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
+        let users = await User.find({});
+        
+        return res.render('home',{
+            title : 'codeComm | Home',
+            all_posts: posts,
+            all_users: users
+        });     
+    }catch(error){
+        console.log('error: ',error);
+        return;
+    }
+    
 }
