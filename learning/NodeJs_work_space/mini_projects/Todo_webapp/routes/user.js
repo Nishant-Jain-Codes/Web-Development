d272e8d68581f7dbbf94f10eb9049a57d0aaccba
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-01-23 05:21:52 +0530

    setup user basic page

--- learning/NodeJs_work_space/mini_projects/Todo_webapp/routes/user.js
+++ learning/NodeJs_work_space/mini_projects/Todo_webapp/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-// const usersController = require('../controllers/user_controller');
-// router.get('/',usersController.user);
+const usersController = require('../controllers/user_controller');
+router.get('/',usersController.user);
 // router.get('/sign-in',usersController.sign_in);
 // router.get('/sign-up',usersController.sign_up);
 // router.get('/sign-out',usersController.sign_out);
