983e89de3698c479597d6b0136d0ff6aece777ee
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-01-22 14:32:15 +0530

    setup layout and assets

--- learning/NodeJs_work_space/mini_projects/Todo_webapp/index.js
+++ learning/NodeJs_work_space/mini_projects/Todo_webapp/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts'); 
 const port = 8000;
 const app = express();
-//setting up view engine and views source
+app.set(express.static('./assets'))
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+app.use(expressLayouts);
 app.set('view engine','ejs');
 app.set('views','./views')
 app.use('/',require('./routes'));
