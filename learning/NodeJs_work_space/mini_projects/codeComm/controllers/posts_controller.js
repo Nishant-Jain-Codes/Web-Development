828667670e38d0b34c50cf9dca41791db3d8936d
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-01-27 13:48:41 +0530

    applying json using ajax

--- learning/NodeJs_work_space/mini_projects/codeComm/controllers/posts_controller.js
+++ learning/NodeJs_work_space/mini_projects/codeComm/controllers/posts_controller.js
@@ -10,10 +10,18 @@ module.exports.analytics = function(req,res){
 }
 module.exports.create = async function(req,res){
     try{
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
+        if(req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post created!"
+            });
+        }
         req.flash('success','post created');
         return res.redirect('back');
 
