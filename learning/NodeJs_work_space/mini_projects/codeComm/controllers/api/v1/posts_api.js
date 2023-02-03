a935b179eae3696558c2d60376e81cf082a77c36
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-02-03 16:41:41 +0530

    siU "

--- learning/NodeJs_work_space/mini_projects/codeComm/controllers/api/v1/posts_api.js
+++ learning/NodeJs_work_space/mini_projects/codeComm/controllers/api/v1/posts_api.js
@@ -1,7 +1,46 @@
+const Post = require('../../../models/post')
+const Comment = require('../../../models/comment')
 //index is the name use when you want to list down something 
-module.exports.index = function(req,res){
+module.exports.index = async function(req,res){
+    let posts = await Post.find({})
+        .sort('--createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            sort: '--createdAt',
+            populate: {
+                path: 'user'
+            }
+        });
+    //remove the users password from the api
+    for(let post of posts){
+        post.user.password = null;
+        if(post.comments){
+            for(let comment of post.comments){
+                comment.user.password = null
+            }
+        }
+    }
     return res.status(200).json({
-        post: [],
+        posts: posts,
         message: "list of posts"
     });
+}
+module.exports.destroy = async function(req,res){
+    
+    try{
+        let post = await Post.findById(req.params.id);
+        console.log('post',post);        
+        post.remove();
+        await Comment.deleteMany({post: req.params.id});
+        return res.json(200,{
+            message: "post and associated comments deleted successfully"
+        });
+    }catch(error){
+        console.log('error in destroying post ',error);
+        return res.json(200,{
+            message: "internal server error"
+        });
+    }
+
 }
