836848f804a2935b623dd9790912c0cc2d470123
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-02-04 12:48:18 +0530

    creating a token successfull

---
+++ learning/NodeJs_work_space/mini_projects/codeComm/controllers/api/v1/users_api.js
@@ -0,0 +1,27 @@
+const User = require('../../../models/user');
+const jwt = require('jsonwebtoken');//generates the Json Web Token
+
+module.exports.createSession = async function(req,res){
+    try {
+        console.log('createSession api called');
+        let user = await User.findOne({email:req.body.email});    
+        if(!user||user.password!=req.body.password){
+            return res.json(422,{
+                message: 'invalid username or password'
+            });
+        }
+        else{
+            return res.json(200,{
+                message: 'sign in successfully : here is your token',
+                data: {
+                    token : jwt.sign((user.toJSON()),'codeComm',{expiresIn: 10000*60})
+                }
+            });
+        }
+    } catch (error) {
+        console.log('error in creating session - user_api ',error);
+        return res.json(200,{
+            message: "internal server error"
+        });
+    }
+}
