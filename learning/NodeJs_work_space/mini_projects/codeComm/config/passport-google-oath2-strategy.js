8594a43c6abfaf113f2e63ac2b38c7a615c99263
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-02-07 17:24:57 +0530

    configured passport for google auth

--- learning/NodeJs_work_space/mini_projects/codeComm/config/passport-google-oath2-strategy.js
+++ learning/NodeJs_work_space/mini_projects/codeComm/config/passport-google-oath2-strategy.js
@@ -1,3 +1,36 @@
 const passport = require('passport');
 const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const crypto = require('crypto');
+const User = require('../models/user');
+passport.use(new googleStrategy({
+    clientID :"1071476209247-b2ift9jeihsulgnq1f0i983f294ur96j.apps.googleusercontent.com",
+    clientSecret :"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",
+    callbackURL: "https://localhost:8000/users/auth/google/callback"
+},function(accessToken,refreshToken,profile,done){
+    User.findOne({email: profile.emails[0].value}).exec(function(error,user){
+        if(error){
+            console.log('error in google strategy passport',error);
+            return;
+        }
+        
+        console.log('google auth profile',profile)
+        
+        if(user){
+            //if user found set this user as req.user
+            return done(null,user);
+        }else{
+            //if not found create the user and set it as req.user i.e sign in the user
+            User.create({
+                name: profile.displayName,
+                email: profile.emails[0].value,
+                password: crypto.randomBytes[20].toString('hex')
+            },function(error,user){
+                if(error){
+                    console.log('error in creating user using google strategy',error);
+                    return;
+                }
+                return done(null,user);
+            });
+        }
+    });
+}));
