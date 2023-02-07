96de3166592f65a1cc2d22d61ad97dd911b6bf34
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-02-07 18:11:32 +0530

    making progress

--- learning/NodeJs_work_space/mini_projects/codeComm/routes/users.js
+++ learning/NodeJs_work_space/mini_projects/codeComm/routes/users.js
@@ -10,10 +10,8 @@ router.get('/profile/:id',passport.checkAuthentication,usersController.profile);
 router.post('/update/:id',passport.checkAuthentication,usersController.update);
 router.post('/create',usersController.create);
 //use passport as a middle ware to authenticate
-router.post('/create-session',passport.authenticate(
-    'local',
-    {
-        failureRedirect: '/users/sign-in'
-    },
-),usersController.createSession);
+router.post('/create-session',passport.authenticate('local',{failureRedirect: '/users/sign-in'}),usersController.createSession);
+router.get('/auth/google',passport.authenticate('google',{scope: ['profile','email']}));//scope is the array or data we are requesting from google
+router.get('/auth/google/callback',passport.authenticate('google',{failureRedirect: '/users/sign-in'}),usersController.createSession);
+
 module.exports = router;
