009e1b24f4addc973770c394250c411becb626cc
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-04-23 11:28:34 +0530

    done with vvvvvbasics of angular task

--- learning-frontend-frameworks/angular/codeComm-angular/src/app/posts-list/posts-list.component.ts
+++ learning-frontend-frameworks/angular/codeComm-angular/src/app/posts-list/posts-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { PostsListService } from '../posts-list.service';
 
 @Component({
   selector: 'app-posts-list',
   templateUrl: './posts-list.component.html',
   styleUrls: ['./posts-list.component.scss']
 })
-export class PostsListComponent {
-
+export class PostsListComponent implements OnInit {
+  posts: any[] = [];
+  constructor(private postsListService: PostsListService) { }
+  ngOnInit(): void {
+    this.getPosts();
+  }
+  getPosts():void {
+    this.postsListService.getPosts().subscribe((response) => (this.posts = response.posts));
+  }
 }
