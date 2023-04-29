786de00babfd0f2dd3c1dd8f35cfa7183d7f004d
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-04-29 19:39:05 +0530

    set up postsList component

--- learning-frontend-frameworks/vue/codecomm-vue/src/App.vue
+++ learning-frontend-frameworks/vue/codecomm-vue/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PostsList/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import PostsList from './components/PostsList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    PostsList,
   }
 }
 </script>
---
+++ learning-frontend-frameworks/vue/codecomm-vue/src/components/PostsList.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="hello">
+    <h1>This is the Posts List</h1>
+</div>
+</template>
+
+<script>
+export default {
+name: 'PostsList',
+}
+</script>
+
+<style scoped>
+h1{
+    font-size: 5vh;
+    color: orangered;
+}
+</style>
