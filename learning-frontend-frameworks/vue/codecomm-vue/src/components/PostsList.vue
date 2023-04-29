d25f053eab536af4dd6e67b4b4f05d95511bb3f0
Nishant-Jain-Codes <mailidfornishantjain@gmail.com>
2023-04-29 19:57:45 +0530

    completed vue cn tute

--- learning-frontend-frameworks/vue/codecomm-vue/src/components/PostsList.vue
+++ learning-frontend-frameworks/vue/codecomm-vue/src/components/PostsList.vue
@@ -1,12 +1,28 @@
 <template>
 <div class="hello">
     <h1>This is the Posts List</h1>
+    <ul>
+        <li v-for="post in posts" :key="post._id" class="post-item">
+            {{ post.content }} -> {{ post.user.name  }}
+        </li>
+    </ul>
 </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-name: 'PostsList',
+    name: 'PostsList',
+    data(){
+        return {
+            posts:[]
+        }
+    },
+    mounted(){
+        axios.get('http://localhost:8000/api/v1/posts').then(Response=>{
+            this.posts = Response.data.posts;
+        });
+    }
 }
 </script>
 
@@ -14,5 +30,12 @@ name: 'PostsList',
 h1{
     font-size: 5vh;
     color: orangered;
+}
+li {
+    font-size: 3vh;
+    color: blue;
+}
+ul{
+    list-style-type: none;
 }
 </style>
